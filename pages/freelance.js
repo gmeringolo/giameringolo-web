56de71319817cfd63a757d69c8203fd007b30b24
Gianella Meringolo <gmeringolo@gmail.com>
2024-12-20 00:56:05 -0300

    sobre-mi

--- pages/freelance.js
+++ pages/freelance.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Layout  from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
 import infoProd from '../public/img/infoProd.png';
 import posadaHorizonte from '../public/img/posadaHorizonte.png';
 
@@ -12,29 +11,29 @@ export default function freelance() {
   return (
     <Layout>
       <Head>
-        <title className={utilStyles.headingTitle}>Freelance</title>
+        <title  class="headingTitle">Freelance</title>
       </Head>
-      <section className={utilStyles.sectionImg}>
-        <div className={utilStyles.voluntImg}>
+      <section class="flex justify-center items-center mt-10">
+        <div class="flex justify-center flex-col items-center">
           <Image
             priority
             src={posadaHorizonte}
-            height={300}
+            height={200}
             alt=""
           />
-          <h3>POSADA HORIZONTE
+          <h3 class="mt-5">POSADA HORIZONTE
           </h3>
-          <p className={utilStyles.pTxt}>Diseño arquitectónico- Diseño de marca - Diseño y desarrollo web</p>
+          <p class="pTxt mt-2 w-62 text-center">Diseño arquitectónico- Diseño de marca - Diseño y desarrollo web</p>
         </div>
-        <div className={utilStyles.voluntImg}>
+        <div class="flex justify-center flex-col items-center">
           <Image
             priority
             src={infoProd}
-            height={300}
+            height={200}
             alt=""
           />
-          <h3>INFO PROD</h3>
-          <p className={utilStyles.pTxt}>Caso de estudio final de carrera Diseño UX/UI </p>
+          <h3 class="mt-5">INFO PROD</h3>
+          <p class="pTxt mt-2 w-62 text-center">Caso de estudio final de carrera Diseño UX/UI </p>
         </div>
        
       </section>

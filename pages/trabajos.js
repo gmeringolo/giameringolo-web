38271324958e7a86fc307f6c7e72d6d5216aa6be
Gianella Meringolo <gmeringolo@gmail.com>
2025-02-18 01:30:28 -0300

    Trabajos

--- pages/trabajos.js
+++ pages/trabajos.js
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Layout  from '../components/layout';
-import infoProd from '../public/img/infoProd.png';
-import posadaHorizonte from '../public/img/posadaHorizonte.png';
-
-
-
+import Layout from '../components/layout';
+import infoProd from '../public/img/infoprod.png';
+import posada from '../public/img/posada.png';
+import salvica from '../public/img/salvica.png';
+import Link from 'next/link';
 
 export default function trabajos() {
   return (
@@ -13,29 +12,58 @@ export default function trabajos() {
       <Head>
         <title class="headingTitle">Trabajos</title>
       </Head>
-      <section class="flex justify-center items-center mt-10">
-        <div class="flex justify-center flex-col items-center">
-          <Image
-            priority
-            src={posadaHorizonte}
-            height={200}
-            alt=""
-          />
-          <h3 class="mt-5">POSADA HORIZONTE
-          </h3>
-          <p class="pTxt mt-2 w-62 text-center">Diseño arquitectónico- Diseño de marca - Diseño y desarrollo web</p>
+      <section class="flex flex-col justify-center items-center mt-10 mx-auto">
+        <div class="flex flex-col justify-center items-center mb-12">
+          <Link href="https://www.behance.net/gallery/115905061/Diseno-UXUI-Buscador-Nutricional" target='_blank' class="flex justify-center">
+            <Image
+              priority
+              src={infoProd}
+              class="w-3/4"
+              alt="proyecto infoprod"
+            />
+          </Link>
+          <div class="flex flex-row pt-5 w-3/4">
+            <h3 class="font-inter_tight font-bold text-xl w-1/3 pl-2">INFOPROD</h3>
+            <div class="w-2/3">
+              <p class="font-inter_tight text-lg font-semibold mb-2">Diseño UX/UI- Caso de estudio</p>
+              <p class="font-inter_tight text-base">Este proyecto busca facilitar el acceso a la información nutricional de alimentos envasados, permitiendo a los usuarios consultar datos a través del código de barras, nombre del producto o empresa. La aplicación también ofrece recetas caseras para elaborar el producto o ideas para usarlo como ingrediente, promoviendo un consumo más saludable y consciente.</p>
+            </div>
+          </div>
         </div>
-        <div class="flex justify-center flex-col items-center">
-          <Image
-            priority
-            src={infoProd}
-            height={200}
-            alt=""
-          />
-          <h3 class="mt-5">INFO PROD</h3>
-          <p class="pTxt mt-2 w-62 text-center">Caso de estudio final de carrera Diseño UX/UI </p>
+        <div class="flex flex-col justify-center items-center mb-12">
+          <Link href="https://www.behance.net/gallery/117132533/Proyecto-Arquitectonico-con-diseno-web-marca-y-redes" target='_blank' class="flex justify-center">
+            <Image
+              priority
+              src={posada}
+              class="w-3/4"
+              alt="proyecto Posada Horizonte"
+            />
+          </Link>
+          <div class="flex flex-row pt-5 w-3/4">
+            <h3 class="font-inter_tight font-bold text-xl w-1/3 pl-2">POSADA HORIZONTE</h3>
+            <div class="w-2/3">
+              <p class="font-inter_tight text-lg font-semibold mb-2">Proyecto arquitectónico y Diseño web responsive</p>
+              <p class="font-inter_tight text-base">Este proyecto se centra en el diseño de viviendas temporales, creadas específicamente para ofrecer comodidad e independencia a sus usuarios. Además, se desarrolló una página web con un enfoque mobile-first y diseño responsive, dado que el público objetivo utiliza predominantemente dispositivos móviles en lugar de computadoras de escritorio. La experiencia de usuario fue una prioridad en cada fase del proyecto, asegurando una navegación intuitiva y fluida en cualquier dispositivo.</p>
+            </div>
+          </div>
         </div>
-       
+        <div class="flex flex-col justify-center items-center mb-12">
+          <Link href="https://www.behance.net/gallery/112077671/Diseno-de-marca-Salvica" target='_blank' class="flex justify-center">
+            <Image
+              priority
+              src={salvica}
+              class="w-3/4"
+              alt="proyecto Salvica"
+            />
+          </Link>
+          <div class="flex flex-row pt-5 w-3/4">
+            <h3 class="font-inter_tight font-bold text-xl w-1/3 pl-2">SALVICA STORE</h3>
+            <div class="w-2/3">
+              <p class="font-inter_tight text-lg font-semibold mb-2">Proyecto de identidad de marca</p>
+              <p class="font-inter_tight text-base">Este proyecto consistió en el desarrollo de la identidad de marca para la comercialización de sal del Himalaya. Se diseñaron todos los materiales gráficos, incluyendo el logo y las imágenes para redes sociales, con el objetivo de crear una presencia visual coherente y atractiva para la marca.</p>
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   );

2a9d1a58c1cbde86e22e846dd63d1d20cc9cb7c9
Gianella Meringolo <gmeringolo@gmail.com>
2025-03-22 16:10:26 -0300

    responsive mobile home

--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,21 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section class="flex justify-center items-center mt-12">
-        <div class="flex justify-center items-center">
+        <div class="flex flex-col justify-center items-center lg:flex-row">
           <Image
             priority
             src={foto}
-            height={300}
-            width={300}
-            class="rounded-full"
+            class="rounded-full w-[250px] h-[250px] lg:w-[300px] lg:h-[300px]"
             alt="Foto Gianella Meringolo"
           />
-          <div class="ml-12">
-            <p class="font-inter_tight text-dark text-xl font-bold">Hola</p>
+          <div class="ml-1 sm:ml-12 md:ml-16 lg:ml-12 mr-1 sm:mr-6 md:mr-10 lg:mr-2 ">
+            <p class="font-inter_tight text-dark text-xl font-bold mt-8 lg:mt-1">Hola</p>
             <p class="font-inter_tight text-dark text-base/7 mt-2">Mi nombre es Gianella Meringolo, soy Uruguaya y actualmente vivo en Montevideo. 
             <br/>En el 2022 culmine la Licenciatura en tecnologías de la información, además soy Diseñadora de interiores, estudié Arquitectura 8 años y en el 2021 me certifique en diseño UX/UI.
             </p>
             <p class="font-inter_tight text-dark text-base/7 mt-2">Desde que comence a trabajar en tecnología, desempeñe roles como Java developer, web ui developer, diseñadora de interfaz de usuario y Analista Genexus. Actualmente trabajo freelance como diseñadora gráfica y desarrollando páginas web.</p>
             <p class="font-inter_tight text-dark text-base/7  mt-2">Soy una persona organizada, responsable, detallista, proactiva, empática, comprometida, con buena comunicación, buena trabajando en equipo y con facilidad de aprendizaje y adaptación.</p>
-            <div class="flex flex-row items-center mt-6">
+            <div class="flex flex-row items-center mt-10 lg:mt-6">
               <Link href="/experiencia" class="group bg-pale p-4 shadow-xl rounded-3xl w-[105px] flex flex-col items-center mr-5 hover:bg-rose-50 ">
                 <MdPerson size={40} class="text-primary" />
                 <p class="font-inter_tight text-dark text-sm hover:text-primary hover:font-semibold">Experiencia</p>
